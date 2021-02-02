b32dacb6500f14b78fdf1e3355c5baddb9a678f7
zgy <qbjooo@qq.com>
2021-02-02 15:38:54 +0800

    补充requirements，添加测试文件

---
+++ tests/test_datamanage.py
@@ -0,0 +1,26 @@
+import unittest
+import os, shutil
+
+HOME = os.environ["HOME"]
+DATA_DIR = os.path.abspath("./testdir")
+
+class TestDataManager(unittest.TestCase):
+    def setUp(self) -> None:
+        os.makedirs(DATA_DIR)
+    
+    def tearDown(self) -> None:
+        shutil.rmtree(DATA_DIR)
+    
+    def test_load(self):
+        from ink.config import config
+        from ink.data import load
+
+        config.path = [ DATA_DIR ]
+        self.assertTrue(load("vocab").startswith(DATA_DIR))
+    
+        with self.assertRaises(ValueError):
+            load("unknown")
+    
+
+
+    
