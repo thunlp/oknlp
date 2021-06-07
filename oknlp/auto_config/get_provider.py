824e4577f8676e5f1a8fba540b44d125f76b5278
unknown <luyanjun1116@163.com>
2021-06-07 22:40:29 +0800

    修改代码中存在的问题

--- oknlp/auto_config/get_provider.py
+++ oknlp/auto_config/get_provider.py
@@ -1,3 +1,4 @@
+import re
 from .gpu_scheduler import get_gpu_info, get_gpu_utilization, get_gpumem_utilization
 
 def get_provider(device=None):
@@ -8,28 +9,31 @@ def get_provider(device=None):
             })
     d_cpu = 'CPUExecutionProvider'
     fp16_mode = False
+
+    gpu_rates = [i['gpu_id'] for i in get_gpu_utilization() if i['gpu_rate'] <0.2]
+    mem_rates = [i['gpu_id'] for i in get_gpumem_utilization() if i['mem_rate'] >0.7]
+    gpu_usable = list(set(gpu_rates).intersection(set(mem_rates)))
+    gpu_info = get_gpu_info(gpu_usable)
+    fp16_device = [i['gpu_id'] for i in gpu_info if i['gpu_compt']>=6.1]
+
     if device==None:
         if get_gpu_info() == []:
             device = 'cpu'
         else:
-            gpu_rates = [i['gpu_id']for i in get_gpu_utilization() if i['gpu_rate'] <0.2]
-            mem_rates = [i['gpu_id'] for i in get_gpumem_utilization() if i['mem_rate'] >0.7]
-            gpu_usable = list(set(gpu_rates).intersection(set(mem_rates)))
-            gpu_info = get_gpu_info(gpu_usable)
-            fp16_device = [i['gpu_id'] for i in gpu_info if i['gpu_compt']>=7.0]
-            #device = 'cuda: {}'.format(fp16_device[0])
+            if len(fp16_device) !=0:
+                device = 'cuda: {}'.format(fp16_device[0])
+            elif len(gpu_usable) !=0:
+                device = 'cuda: {}'.format(gpu_usable[0])
+            else:
+                device = 'cpu'
+
     if device == 'cpu':
         providers = [d_cpu]
-    elif device != None:
-        d_cuda[1]['device_id'] = device[-1]
-        if int(device[-1]) in fp16_device:
+    elif 'cuda' in device:
+        pattern = re.compile(r'\d+')    
+        span = re.search(pattern, device).span() 
+        d_cuda[1]['device_id'] = int(device[span[0]:span[1]])
+        if d_cuda[1]['device_id']) in fp16_device:
             fp16_mode = True
-        providers = [d_cuda, d_cpu]
-    else:
-        if fp16_device != []:
-            d_cuda[1]['device_id'] = fp16_device[0]
-            fp16_mode = True
-        else:
-            d_cuda[1]['device_id'] = gpu_usable[0]
         providers = [d_cuda, d_cpu]
     return providers, fp16_mode 
--- oknlp/nn/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .bertlinear import BertLinear
-from .bertseq import BertSeq
-from .bertlinearsigmoid import BertLinearSigmoid
