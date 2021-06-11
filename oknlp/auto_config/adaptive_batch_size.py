83173510504496760a8fc44e095ee233b3e6080f
Yumiko-188 <luyanjun1116@163.com>
2021-06-11 16:47:05 +0800

    添加batch_size大小自动设置和mem分配

---
+++ oknlp/auto_config/adaptive_batch_size.py
@@ -0,0 +1,15 @@
+import py3nvml
+
+def adaptive_batch_size(gpuid, fp16_mode):
+    py3nvml.py3nvml.nvmlInit()
+    h = py3nvml.py3nvml.nvmlDeviceGetHandleByIndex(gpuid)
+    info = py3nvml.utils.try_get_info(py3nvml.py3nvml.nvmlDeviceGetMemoryInfo, h,
+                             ['something'])
+    mem_avl = info.free>>30
+    if fp16_mode == True:
+        batch_size = mem_avl * 16
+    else:
+        batch_size = mem_avl * 4
+
+    py3nvml.py3nvml.nvmlShutdown()
+    return batch_size, mem_avl
